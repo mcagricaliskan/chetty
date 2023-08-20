45b1795d2dabf2a2f38586180bf14ae66c927b89
Çağrı Çalışkan <mcagricaliskan@gmail.com>
2023-08-20 22:49:57 +0300

    update isuser exists query for real one & add dummpy create user

--- controllers/auth/storage.go
+++ controllers/auth/storage.go
@@ -6,7 +6,13 @@ import (
 )
 
 func IsUserExists(db *postgres.Postgres, ctx context.Context, username string) (isUserExists bool, err error) {
-	err = db.Connection.QueryRow(ctx, `select when case count(*) > 0 than false else true from thegame.users where username = $1`, username).Scan(&isUserExists)
+	err = db.Connection.QueryRow(ctx, `select 
+			case 
+				when count(*) = 1 then true
+				else false
+			end
+		from thegame.users u where u.username = $1`,
+		username).Scan(&isUserExists)
 	return isUserExists, err
 }
 
@@ -15,4 +21,9 @@ func GetUserPassword(db *postgres.Postgres, ctx context.Context, username string
 		`select when case count(*) > 0 than false else true from thegame.users where username = $1`, username).Scan(&isUserExists, &password)
 	return isUserExists, password, err
 
+}
+
+func CreateUser(db *postgres.Postgres, ctx context.Context, user *RegisterReq, hashedPassword string) error {
+	_, err := db.Connection.Exec(ctx, `insert into thegame.users (username, password) values ($1, $2)`, user.Username, hashedPassword)
+	return err
 }
