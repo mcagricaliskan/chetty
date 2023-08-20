f474fecbbf3858f43ba14176212e1d0e85f727e0
Çağrı Çalışkan <mcagricaliskan@gmail.com>
2023-08-20 23:24:55 +0300

    ad jwt middleware for endpoints other than auth

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"the-game-backend/controllers/auth"
 	"the-game-backend/services/postgres"
 
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
@@ -26,7 +27,11 @@ func main() {
 	app := fiber.New()
 
 	auth := auth.Authentication{DB: db}
-	auth.Router(app)
+	auth.RegisterRouters(app)
+
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte(os.Getenv("JWT_SECRET"))},
+	}))
 
 	log.Fatal(app.Listen(":33333"))
 }
