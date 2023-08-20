969d9b1e0c77dca903b77d370c57eccb369473e6
Çağrı Çalışkan <mcagricaliskan@gmail.com>
2023-08-20 22:50:16 +0300

    add load dot env

--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"log"
+	"os"
 	"the-game-backend/controllers/auth"
 	"the-game-backend/services/postgres"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
+	if os.Getenv("ENVIROMENT") != "PRODUCTION" {
+		godotenv.Load()
+	}
+
 	db, err := postgres.Connect()
 	if err != nil {
 		log.Fatal("Database Connection Can't Estabilished, Error:", err)
+	} else {
+		log.Println("Database Connection Estabilished")
 	}
 
 	app := fiber.New()
