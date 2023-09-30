990bd708a8f0ab1a65063cb3d88c4f3f9ff29555
Çağrı Çalışkan <mcagricaliskan@gmail.com>
2023-09-30 20:31:35 +0300

    add mock create user

--- internal/auth/mock_repository.go
+++ internal/auth/mock_repository.go
@@ -1,15 +1,24 @@
 package auth
 
+type User struct {
+	Id         int    `json:"user_id"`
+	UserName   string `json:"user_name"`
+	DiplayName string `json:"display_name"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+}
+
 // MockRepository is a mock implementation of the Repository interface.
 type MockRepository struct {
 	// Users is a map of user IDs to users.
-	Users map[string]*User
+	Users  map[int]*User
+	LastID int
 }
 
 // NewMockRepository returns a new instance of MockRepository.
 func NewMockRepository() *MockRepository {
 	return &MockRepository{
-		Users: make(map[string]*User),
+		Users: make(map[int]*User),
 	}
 }
 
@@ -21,4 +30,18 @@ func (r *MockRepository) IsUserExists(userName string, userEmail string) (isUser
 		}
 	}
 	return false, nil
+}
+
+// CreateUser creates a new user.
+func (r *MockRepository) CreateUser(userName string, displayName string, email string, hashedPassword string) error {
+	r.LastID++
+	user := &User{
+		Id:         r.LastID,
+		UserName:   userName,
+		DiplayName: displayName,
+		Email:      email,
+		Password:   hashedPassword,
+	}
+	r.Users[user.Id] = user
+	return nil
 }
