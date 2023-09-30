1f6a62f89d72ed7c4ff8ce199467c6e9ec0524c8
Çağrı Çalışkan <mcagricaliskan@gmail.com>
2023-09-30 18:42:41 +0300

    add new validators

--- internal/auth/utils.go
+++ internal/auth/utils.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"errors"
 	"fmt"
+	"regexp"
 	"unicode"
 
 	"golang.org/x/crypto/bcrypt"
@@ -54,5 +55,23 @@ func validatePassword(password string) error {
 		return errors.New("password must have at least one special character")
 	}
 
+	return nil
+}
+
+// ValidateEmail checks if the provided string is a valid email.
+func ValidateEmail(email string) error {
+	const emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+	if !re.MatchString(email) {
+		return errors.New("provided string is not a valid email")
+	}
+	return nil
+}
+
+// ValidateUsername checks if the provided string is a valid username (more than 3 characters).
+func ValidateUsername(username string) error {
+	if len(username) <= 3 {
+		return errors.New("username must be longer than 3 characters")
+	}
 	return nil
 }
