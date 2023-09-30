614ada0c2b2a59ea397e81ded4228a7a266ddf9b
Çağrı Çalışkan <mcagricaliskan@gmail.com>
2023-09-30 18:39:11 +0300

    add password validate func

--- internal/auth/utils.go
+++ internal/auth/utils.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
+	"unicode"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -12,4 +16,43 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func validatePassword(password string) error {
+	const minPasswordLength = 8
+	if len(password) < minPasswordLength {
+		return fmt.Errorf("password must be at least %d characters long", minPasswordLength)
+	}
+
+	var (
+		hasUppercase, hasLowercase, hasDigit, hasSpecial bool
+	)
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUppercase = true
+		case unicode.IsLower(char):
+			hasLowercase = true
+		case unicode.IsDigit(char):
+			hasDigit = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+
+	if !hasUppercase {
+		return errors.New("password must have at least one uppercase letter")
+	}
+	if !hasLowercase {
+		return errors.New("password must have at least one lowercase letter")
+	}
+	if !hasDigit {
+		return errors.New("password must have at least one digit")
+	}
+	if !hasSpecial {
+		return errors.New("password must have at least one special character")
+	}
+
+	return nil
 }
